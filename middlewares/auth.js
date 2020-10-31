56a90154cfd37b69b35af48ab2646dd728fc8128
Stas Mikhailov <39404498+Haugety@users.noreply.github.com>
2020-10-31 14:57:09 +0300

    Update auth.js
    fix: auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const UnauthorizedError = require('../errors/unauthorized-error');
 module.exports = (req, res, next) => {
   const { authorization = '' } = req.headers;
 
-  if (!authorization && !authorization.startsWith('Bearer ')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Необходима авторизация');
   }
 
